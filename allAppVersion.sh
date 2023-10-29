10880c86871ea93aa8d70599d29688213cc4013c
ROBIND KUMAR <kumarrobind386@gmail.com>
2023-10-30 00:04:21 +0530

    shell script to print version of all installed apps

---
+++ allAppVersion.sh
@@ -0,0 +1,9 @@
+#!/opt/homebrew/bin/bash
+
+echo "Bash ${BASH_VERSION}"
+echo "$(clang --version)"
+echo "$(java --version)"
+echo "$(python3 --version)"
+echo "Node $(node --version), npm $(npm --version)"
+echo "$(git --version)"
+
